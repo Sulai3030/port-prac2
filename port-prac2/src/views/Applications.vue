991d364ca3e5c18b09ec04c3f062def1621a446c
sully34 <sssulai@gmail.com>
2020-09-21 15:17:29 -0400

    first commit

---
+++ port-prac2/src/views/Applications.vue
@@ -0,0 +1,132 @@
+<template>
+  <b-container fluid class="text-light text-center">
+    <b-row class="mb-3" align-h="start">
+      <b-col md="4" class="p-3 bg-info" float-left
+        >Project 1.
+        <p>
+          Party Dash|
+          <a href="https://partydash.com/" target="_blank"
+            >https://partydash.com/</a
+          >
+        </p>
+        <p>Front-End Design</p>
+        We built Party Dash with a simple goal in mind: to help party hosts plan
+        parties more easily. easily.
+        <p>
+          Responsibilities: Follow design specs handed down from site owner.
+          Used CSS and HTML to create inviting website
+        </p>
+
+        <p>Technologies used: HTML, CSS, Bootstrap</p>
+        <div>
+          <a href="https://partydash.com/pages/about-us" target="_blank"
+            >More Info</a
+          >
+        </div>
+      </b-col>
+      <br />
+      <b-col md="4" class="ml-auto p-3 bg-info"></b-col>
+    </b-row>
+    <b-row class="mb-3">
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="auto" class="mr-auto p-3 bg-info"></b-col>
+      <b-col cols="auto" class="p-3 bg-info"></b-col><br />
+    </b-row>
+    <b-row class="mb-3" align-h="start">
+      <b-col md="4" class="p-3 bg-info" float-left
+        >Project 2.
+        <p>
+          RuneSkype
+          <a href="https://runeskype.heroku.app/" target="_blank"
+            >https://github.com/Jroze88/RuneSkype</a
+          >
+        </p>
+        <p>
+          Front-end design, Linting, cleaning up frontend and backend code and
+          making sure both sides work together.
+        </p>
+        <p>
+          RuneSkype is the newest way for friends to interact while playing your
+          favorite Role Playing Game (RPG) RuneSkype. Playing RuneSkype is easy,
+          just download a copy from our homepage or our repo:
+        </p>
+
+        <p>
+          Responsibilities: Wrote clean, presentable code. Kept group on time
+          and on track to present at Project Demo Day.
+        </p>
+        Technologies used: ReactJS, ChanceJS, Node, Express, Firebase,
+        Twilio-Video, Twilio, WebRTC, Axios. Express, Faker, jQuery, React-dnd,
+        React-dnd-html5-backend,React-dom, React-draggable, React-router-dom,
+        Webpack-dev-middleware, Webpack hot-middleware
+
+        <a href="https://github.com/Sulai3030/RuneSkype" target="_blank"
+          >More Info</a
+        > </b-col
+      ><br /><br />
+      <b-col md="4" class="ml-auto p-3 bg-info"></b-col>
+    </b-row>
+    <b-row class="mb-3">
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="auto" class="mr-auto p-3 bg-info"></b-col>
+      <b-col cols="auto" class="p-3 bg-info"
+        >Project 3.
+        <p>
+          The GOAT Voter App |
+          <a href="http://www.goatvoter.org/" target="_blank"
+            >http://www.goatvoter.org/</a
+          >
+        </p>
+        <p>
+          Front-End Design, created the CSS color scheme and front-end
+          components.
+        </p>
+        The GOAT Voter App is a one-stop platform where voters can access
+        information about candidates running for political offices anywhere in
+        the United States. The GOAT Voter App streamlines information and
+        increases voter participation. repo: By entering their zip code in a
+        search field, users will be able to search and access detailed
+        information about all candidates running for election in their district.
+        In addition, users can enter their address to find out where their
+        nearest polling site is located and request transportation on election
+        day. Technologies used: JavaScript, Handlebars, Bootstrap, CSS Parallax,
+        Node.js, Express, Sequelize, Google Civic Information API, Heroku </b-col
+      ><br /><br />
+    </b-row>
+    <b-row class="mb-3">
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+      <b-col md="3" class="ml-md-auto p-3 bg-info"></b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="auto" class="mr-auto p-3 bg-info"></b-col>
+      <b-col cols="auto" class="p-3 bg-info"
+        >Project 4.
+        <p>
+          Beer Near Here |
+          <a href="https://github.com/Christiantav/beerNearHere" target="_blank"
+            >Beer Near Here App</a
+          >
+        </p>
+        <p>Backend Design.</p>
+        Beer Near Here is an app that services beer drinkers in and around the
+        New York and New Jersey areas using user generated information via
+        questionnaire about beer preferences returning locations that serve
+        craft beers.
+        <p>
+          Technologies Used: HTML5, CSS, and JavaScript, Body-parser, Express,
+          Bootstrap, CSS Parallax, GitHub Pages
+        </p> </b-col
+      ><br />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
